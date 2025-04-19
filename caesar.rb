83bb56f8456a18401e71e439a3034525320a769a
reuben <reubegan@gmail.com>
2025-04-19 10:14:12 +0100

    do caesar

--- caesar.rb
+++ caesar.rb
@@ -1,4 +1,19 @@
-
 def encode(string, wrap)
-  string
+  string.split('').collect{|char|
+  if char.ord.between?(65, 90) || char.ord.between?(97, 122) then  
+    shift_char(char, wrap) 
+  else 
+    char 
+  end
+}.join
 end
+
+def shift_char(char, shift)
+  newchar = char.ord + shift
+  if  !newchar.between?(65, 90) && !newchar.between?(97,122)
+    newchar -= 26
+  end
+  newchar.chr
+end
+
+p encode('xyzabc', 5)
